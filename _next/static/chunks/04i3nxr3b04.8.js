5973fce56f01dca6482d2f4f0123f7d07184490d
paambaati <exchequer598@gmail.com>
2026-04-28 06:22:13 +0000

    Deploying to gh-pages from @ paambaati/terrably@be14bf1d7635f22847adc6aca924b3a4b01ce9c7 🚀

---
+++ _next/static/chunks/04i3nxr3b04.8.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,21952,t=>{"use strict";let e=(0,t.i(38722).default)("chevron-right",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);t.s(["ChevronRight",0,e],21952)}]);
